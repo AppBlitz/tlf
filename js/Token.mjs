83c8eb50dcbf64b34aea8cb5d2178a2f2169e66a
Carlos Fabián Corrales Zapata <166270892+AppBlitz@users.noreply.github.com>
2025-05-10 13:24:08 -0500

    Create Token.mjs

---
+++ js/Token.mjs
@@ -0,0 +1,32 @@
+/**
+ * Clase que representa un Token, que contiene una expresión, categoría e índice.
+ * 
+ * @class Token
+ */
+class Token {
+
+  /**
+   * Crea una instancia de la clase Token.
+   * 
+   * @param {string} expresion - La expresión asociada al token.
+   * @param {string} category - La categoría o tipo del token.
+   * @param {number} index - El índice de la posición del token.
+   */
+  constructor(expresion, category, index) {
+    this.expresion = expresion;
+    this.category = category;
+    this.index = index;
+  }
+
+  /**
+   * Obtiene el token completo como una cadena de texto.
+   * 
+   * @returns {string} La representación en cadena del token, combinando la expresión, categoría, índice y la palabra "finalizado".
+   */
+  get token() {
+    return this.expresion + "" + this.category + "" + this.index + "finalizado";
+  }
+
+}
+
+export { Token };

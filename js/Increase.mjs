3466d5f3bb7796fd7e8657e1e3aac47928e16090
Joan Gomez <joangomezusma4@gmail.com>
2025-05-26 19:14:15 -0500

    Agregacion de increase

---
+++ js/Increase.mjs
@@ -0,0 +1,33 @@
+/**
+ * Determina el tipo de operador aritmético o de incremento/decremento en una expresión.
+ * 
+ * @param {Array} datas - La expresión en forma de array de caracteres o tokens.
+ * @param {number} index - El índice de la posición del operador dentro de la expresión.
+ * @returns {Token|null} Un objeto Token que representa el operador encontrado, o null si no se encuentra ninguno.
+ */
+function isAritemticoOrDecrement(datas, index) {
+  if (verificationCaracter(datas, "++")) {
+    return new Token(datas, "Operador aumento", index);
+  }
+  else if (verificationCaracter(datas, "--")) {
+    return new Token(datas, "Operador de decremento", index);
+  }
+  else if (verificationCaracter(datas, "+")) {
+    return new Token(datas, "Operador de suma", index);
+  }
+  else if (verificationCaracter(datas, "-")) {
+    return new Token(datas, "Operador de resta", index);
+  }
+  else if (verificationCaracter(datas, "/")) {
+    return new Token(datas, "Operador de división", index);
+  }
+  else if (verificationCaracter(datas, "*")) {
+    return new Token(datas, "Operador de multiplicación", index);
+  }
+  else if (verificationCaracter(datas, "%")) {
+    return new Token(datas, "Operador de modulo", index);
+  }
+  else {
+    return null;
+  }
+}
